800dccf8eb01e007f98748d295c729bc31caf1b7
lana rodion <lana.rodion@gmail.com>
2025-04-30 18:05:20 +0200

    Move icons in_icons.scss + Add vertical tab in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { PageTemplateComponent } from '../../shared/page-template/page-template.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [PageTemplateComponent],
+  imports: [CommonModule, PageTemplateComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   titleHeader: string = 'Home';
+  tab: number = 1;
 }

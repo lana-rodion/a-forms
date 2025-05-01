2283122c999ff64afb57bc08d578f1438050071c
lana rodion <lana.rodion@gmail.com>
2025-05-01 21:21:58 +0200

    Improve page-template + vertical tabs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PageTemplateComponent } from '../../shared/page-template/page-template.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, PageTemplateComponent],
+  imports: [CommonModule, PageTemplateComponent, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   titleHeader: string = 'Home';
-  tab: number = 1;
+  tab: string = 'tdforms';
 }
--- src/app/pages/template-driven-form/template-driven-form.component.ts
+++ src/app/pages/template-driven-form/template-driven-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { PageTemplateComponent } from '../../shared/page-template/page-template.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [PageTemplateComponent],
+  imports: [PageTemplateComponent, RouterLink, RouterLinkActive],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add this line if using Web Components
 })
 export class TemplateDrivenFormComponent {
   titleTemplate: string = 'Template-driven form';

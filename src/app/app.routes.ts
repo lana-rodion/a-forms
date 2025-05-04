743f12c83f8eca6186ef9027309636590b4edc30
lana rodion <lana.rodion@gmail.com>
2025-05-04 18:58:46 +0200

    Clean up app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { TemplateDrivenFormComponent } from './pages/template-driven-form/template-driven-form.component';
-import { ReactiveFormComponent } from './pages/reactive-form/reactive-form.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },

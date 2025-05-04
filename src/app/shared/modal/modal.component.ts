6c81659b9562840847e3ec64b710bb9d58566797
lana rodion <lana.rodion@gmail.com>
2025-05-04 17:30:54 +0200

    Add modal.component

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  @Input() title: string = '';
+  @ViewChild('myModal', { static: false }) modal!: ElementRef;
+  @ViewChild('content', { static: false }) content!: ElementRef;
+
+  open() {
+    this.modal.nativeElement.style.display = 'block';
+  }
+
+  close() {
+    this.modal.nativeElement.style.display = 'none';
+  }
+}

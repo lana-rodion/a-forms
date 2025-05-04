19665a6d00ce000e4d1227060459516455b0fbf5
lana rodion <lana.rodion@gmail.com>
2025-05-04 17:33:09 +0200

    Add modal into reactive-form.component

--- src/app/pages/reactive-form/reactive-form.component.ts
+++ src/app/pages/reactive-form/reactive-form.component.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [CommonModule],
+  imports: [CommonModule, ModalComponent],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.scss',
 })
-export class ReactiveFormComponent {}
+export class ReactiveFormComponent {
+  titleModal = 'Reactive Form Modal';
+  contentModal = 'This is the content of the modal.';
+  footerModal = 'This is the footer of the modal.';
+  @ViewChild('modal', { static: false }) modal!: ModalComponent;
+}

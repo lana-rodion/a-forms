d5d2c26659513d8831c3de457cbeb210e53a48fa
lana rodion <lana.rodion@gmail.com>
2025-05-05 17:04:39 +0200

    Implement TdformValidation Component

--- src/app/components/tdform-validation/tdform-validation.component.ts
+++ src/app/components/tdform-validation/tdform-validation.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { User } from '../../models/user.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatchPasswordDirective } from '../../directives/match-password.directive';
+import { PasswordPatternDirective } from '../../directives/password-pattern.directive';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-tdform-validation',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatchPasswordDirective,
+    PasswordPatternDirective,
+    ModalComponent,
+  ],
   templateUrl: './tdform-validation.component.html',
-  styleUrl: './tdform-validation.component.scss'
+  styleUrl: './tdform-validation.component.scss',
 })
 export class TdformValidationComponent {
+  userModal = new User();
+  titleModal = 'Custom Template-Driven Form Validation';
+  @ViewChild('modal', { static: false }) modal!: ModalComponent;
 
+  constructor() {}
+
+  onSubmit() {
+    confirm(
+      'Form Submitted succesfully!!!\n Check the values in browser console.'
+    );
+    console.table(this.userModal);
+  }
 }

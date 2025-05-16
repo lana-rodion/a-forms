05e39a9fbf22955d89746a5056d2f78e2c6505be
lana rodion <lana.rodion@gmail.com>
2025-05-16 23:49:23 +0200

    Direct Access Using @ViewChild between task-list and project

--- src/app/components/share-data/project/project.component.ts
+++ src/app/components/share-data/project/project.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PageTemplateComponent } from '../../../shared/page-template/page-template.component';
+import { TaskListComponent } from '../task-list/task-list.component';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [CommonModule, RouterLink, PageTemplateComponent, TaskListComponent],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrl: './project.component.scss',
 })
-export class ProjectComponent {
+export class ProjectComponent implements AfterViewInit {
+  titleHeader: string = 'Direct Access Using @ViewChild';
+  @ViewChild(TaskListComponent) taskList!: TaskListComponent;
+  @ViewChild('projectCounter') projectCounter!: ElementRef;
 
+  ngAfterViewInit(): void {
+    if (this.projectCounter && this.projectCounter.nativeElement) {
+      this.projectCounter.nativeElement.textContent = this.taskList.taskCount;
+    }
+  }
 }
--- src/app/components/share-data/task-list/task-list.component.ts
+++ src/app/components/share-data/task-list/task-list.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-task-list',
   imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
 export class TaskListComponent {
-
+  taskCount: number = 15;
 }

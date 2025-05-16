4a6094f4458d9d1cbb62678243197fb3296aa682
lana rodion <lana.rodion@gmail.com>
2025-05-16 13:29:24 +0200

    Add form in child.component

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,20 +1,30 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-child',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss',
 })
 export class ChildComponent {
   // decorate the property with required metadata
-  @Input({ required: true }) childData1!: string;
+  @Input() childData1: string = '';
   @Input() childData2: string = '';
   @Output() newItemEvent = new EventEmitter<string>();
+  newItem: string = '';
+  invalid: boolean = true;
 
   addNewItem(value: string) {
     this.newItemEvent.emit(value);
   }
   // The addNewItem method emits an event with a string value when called.
   // This event can be listened to by the parent component to perform some action.
+  checkInputEmpty() {
+    if (this.newItem.length == 0) {
+      this.invalid = true;
+    } else {
+      this.invalid = false;
+    }
+  }
 }

e0546850f73733af2a32ed243e08fe12ee97936a
lana rodion <lana.rodion@gmail.com>
2025-05-16 13:30:14 +0200

    Modif parent.component

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ChildComponent } from '../child/child.component';
 import { CommonModule } from '@angular/common';
+import { PageTemplateComponent } from '../../shared/page-template/page-template.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-parent',
-  imports: [CommonModule, ChildComponent],
+  imports: [CommonModule, RouterLink, ChildComponent, PageTemplateComponent],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.scss',
 })
 export class ParentComponent {
+  titleHeader: string = 'Parent to Child & Child to Parent';
   parentData1Requiered: string = 'Parent Data1 Requiered';
   parentData2: string = 'Parent Data 2';
   items = ['item1', 'item2', 'item3', 'item4'];

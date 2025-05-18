0bbdb806c089252f4b0ca5b6a6ecfc86d7a327b5
lana rodion <lana.rodion@gmail.com>
2025-05-18 11:19:41 +0200

    Add links to read about localSorage and routeParams

--- src/app/components/share-data/localstorage/localstorage.component.ts
+++ src/app/components/share-data/localstorage/localstorage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PageTemplateComponent } from '../../../shared/page-template/page-template.component';
 
 @Component({
   selector: 'app-localstorage',
-  imports: [],
+  imports: [RouterLink, PageTemplateComponent],
   templateUrl: './localstorage.component.html',
-  styleUrl: './localstorage.component.scss'
+  styleUrl: './localstorage.component.scss',
 })
 export class LocalstorageComponent {
-
+  titleHeader: string = 'Browser Storage for Persistent Data Sharing';
 }
--- src/app/components/share-data/routeparams/routeparams.component.ts
+++ src/app/components/share-data/routeparams/routeparams.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PageTemplateComponent } from '../../../shared/page-template/page-template.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-routeparams',
-  imports: [],
+  imports: [RouterLink, PageTemplateComponent],
   templateUrl: './routeparams.component.html',
-  styleUrl: './routeparams.component.scss'
+  styleUrl: './routeparams.component.scss',
 })
 export class RouteparamsComponent {
-
+  titleHeader: string =
+    'How to pass data between components using routes in Angular?';
 }
